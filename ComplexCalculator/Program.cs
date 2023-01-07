af3fc860611d00b7ba8b08916dd650158251607e
Ariel <luisarielperees@gmail.com>
2023-01-06 20:55:31 -0400

    version 1

---
+++ ComplexCalculator/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Numerics;
+
+namespace ComplexCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Crea dos números complejos
+            Complex c1 = new Complex(1, 3);
+            Complex c2 = new Complex(4, 5);
+
+            // Realiza algunas operaciones con ellos
+            Complex sum = c1 + c2;
+            Complex difference = c1 - c2;
+            Complex product = c1 * c2;
+            Complex quotient = c1 / c2;
+
+            // Muestra los resultados
+            Console.WriteLine($"{c1} + {c2} = {sum}");
+            Console.WriteLine($"{c1} - {c2} = {difference}");
+            Console.WriteLine($"{c1} * {c2} = {product}");
+            Console.WriteLine($"{c1} / {c2} = {quotient}");
+            Console.ReadLine();
+        }
+       
+    }
+}
